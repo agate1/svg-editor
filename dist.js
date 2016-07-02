e447fe3ef9a4b64c52db4ef1f8cc1c0570a23b94
Agnieszka Tomkowicz <agnieszka@uigstudio.com>
2016-07-02 13:20:43 +0200

    xx

--- dist.js
+++ dist.js
@@ -6,12 +6,12 @@ var debug = document.getElementById("debug");
  * Point
  */
 var P = (function () {
-    function P(x, y) {
-        this.x = x;
-        this.y = y;
+    function P(point) {
+        this.x = parseFloat(point.split(",")[0]);
+        this.y = parseFloat(point.split(",")[1]);
     }
     P.prototype.toPath = function () {
-        return this.x + "," + this.y;
+        return this.x.toString() + "," + this.y.toString();
     };
     return P;
 }());
@@ -21,37 +21,16 @@ var P = (function () {
 var SVGPointC = (function () {
     function SVGPointC() {
     }
-    SVGPointC.prototype.add = function (n, p) {
-        switch (n) {
-            case 1:
-                this.cp1 = p;
-                break;
-            case 2:
-                this.p = p;
-                break;
-            case 3:
-                this.cp2 = p;
-                break;
-        }
-    };
     return SVGPointC;
 }());
 /**
  * SVGMPoint
  */
 var SVGPointM = (function () {
-    function SVGPointM() {
+    function SVGPointM(p, cp2) {
+        this.p = p;
+        this.cp2 = cp2;
     }
-    SVGPointM.prototype.add = function (n, p) {
-        switch (n) {
-            case 1:
-                this.p = p;
-                break;
-            case 2:
-                this.cp2 = p;
-                break;
-        }
-    };
     return SVGPointM;
 }());
 /**
@@ -59,18 +38,17 @@ var SVGPointM = (function () {
  */
 var MySVGPath = (function () {
     function MySVGPath(d) {
-        var temp = d.replace(/[A-Z]/g, "");
-        this.cos = temp;
+        var temp = d.replace(/[A-Z]/g, "").split(" ");
+        var cleanArr = temp.filter(function (s) { return s != ""; });
+        var p1 = new P(cleanArr[0]);
+        var p2 = new P(cleanArr[1]);
+        var p = new SVGPointM(p1, p2);
+        this.points = [];
+        this.points.push(p);
     }
-    MySVGPath.prototype.addPathStartPoint = function (p) {
-        this.points.push(p);
-    };
-    MySVGPath.prototype.addPoint = function (p) {
-        this.points.push(p);
-    };
     return MySVGPath;
 }());
 //svg path to points
 var svgpath = new MySVGPath(path);
 //d=" M 190,21 C 213,7 246,10 266,28 C 261,34 256,40 251,46 C 242,38 229,33 216,35 C 210,36 204,40 200,45 C 188,57 189,78 201,90 C 208,97 219,99 229,98 C 233,97 238,95 242,93 C 242,84 242,75 242,65 C 250,65 257,65 265,65 C 265,79 265,93 265,107 C 249,117 230,123 211,120 C 196,118 181,109 173,96 C 164,84 162,68 166,54 C 169,40 178,28 190,21 Z"></path>
-debug.innerText = svgpath.cos;
+debug.innerText = "kk";
--- script.ts
+++ script.ts
@@ -8,16 +8,17 @@ var debug = document.getElementById("debug");
  * Point
  */
 class P {
-	x:string;
-	y:string;
+	x:number;
+	y:number;
 
-	constructor(x, y) {
-		this.x = x;
-		this.y = y;
+	constructor(point:string) {
+		this.x = parseFloat(point.split(",")[0]);
+		this.y = parseFloat(point.split(",")[1]);
 	}
 
+
 	toPath() {
-		return this.x + "," + this.y;
+		return this.x.toString() + "," + this.y.toString();
 	}
 }
 
@@ -29,19 +30,6 @@ class SVGPointC {
 	cp2:P;
 	p:P;
 
-	add(n:number, p:P) {
-		switch (n) {
-			case 1:
-				this.cp1 = p;
-				break;
-			case 2:
-				this.p = p;
-				break;
-			case 3:
-				this.cp2 = p;
-				break;
-		}
-	}
 }
 
 
@@ -49,18 +37,12 @@ class SVGPointC {
  * SVGMPoint
  */
 class SVGPointM {
-	private p:P;
-	private cp2:P;
+	p:P;
+	cp2:P;
 
-	add(n:number, p:P) {
-		switch (n) {
-			case 1:
-				this.p = p;
-				break;
-			case 2:
-				this.cp2 = p;
-				break;
-		}
+	constructor(p:P, cp2:P) {
+		this.p = p;
+		this.cp2 = cp2;
 	}
 }
 
@@ -69,20 +51,21 @@ class SVGPointM {
  */
 class MySVGPath {
 	points:Array<any>;
-	cos:string;
 
 	constructor(d:string) {
-		var temp = d.replace(/[A-Z]/g,"");
-		this.cos = temp;
+		var temp = d.replace(/[A-Z]/g,"").split(" ");
+		var cleanArr = temp.filter(s => s !="");
+		var p1 = new P(cleanArr[0]);
+		var p2 = new P(cleanArr[1]);
+
+		var p = new SVGPointM(p1,p2);
+		this.points=[];
+		this.points.push(p);
+
+
 	}
 
-	addPathStartPoint(p:SVGPointM) {
-		this.points.push(p);
-	}
 
-	addPoint(p:SVGPointC) {
-		this.points.push(p);
-	}
 }
 
 
@@ -91,4 +74,4 @@ var svgpath = new MySVGPath(path);
 
 //d=" M 190,21 C 213,7 246,10 266,28 C 261,34 256,40 251,46 C 242,38 229,33 216,35 C 210,36 204,40 200,45 C 188,57 189,78 201,90 C 208,97 219,99 229,98 C 233,97 238,95 242,93 C 242,84 242,75 242,65 C 250,65 257,65 265,65 C 265,79 265,93 265,107 C 249,117 230,123 211,120 C 196,118 181,109 173,96 C 164,84 162,68 166,54 C 169,40 178,28 190,21 Z"></path>
 
-debug.innerText = svgpath.cos;
+debug.innerText = "kk";
